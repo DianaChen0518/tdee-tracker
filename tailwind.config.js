71fd4c4b1cae0cd38f7796792420c12140c54af0
Jinsiyeon <149858068+DianaChen0518@users.noreply.github.com>
2026-03-28 23:28:34 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // 核心：开启基于 class 的黑暗模式支持
+  darkMode: 'class', 
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
